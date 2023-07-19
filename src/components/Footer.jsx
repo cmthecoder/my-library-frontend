c8ba50267fcfd95d5618ae7f428ecb1a1cf0252d
Chris Mems <christopher.mems@.com>
2023-07-18 22:05:09 -0500

    establish the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="bg-white bg-opacity-75">
+      <div className="container mx-auto py-4 px-6">
+        <div className="text-gray-700 text-sm text-center">
+          &copy; 2023 MyLibrary. All Rights Reserved.
+        </div>
+      </div>
+    </footer>
   )
 }
 
